b33efa2fef5ed3576952687a7b9b133cc8c38ffc
ancy-gg <fpenaofficial@gmail.com>
2026-04-01 23:48:43 +0800

    TSP-v4.2.1-p-mcp

--- include/Pins.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-static constexpr uint8_t IN1_PIN = 1;
-static constexpr uint8_t IN2_PIN = 2;
-static constexpr uint8_t IN3_PIN = 3;
-static constexpr uint8_t IN4_PIN = 4;
